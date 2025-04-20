981e59ec4fe79ad080074b50b4e2f3428c177953
HamzaTheProgrammer23 <yunqpaperboy@hotmail.com>
2025-04-20 15:52:02 -0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int [] testArr = {1,3,5,7,9};
+        printArrValues(testArr);
+    }
+    public static void printArrValues(int[] arr){
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+
+    }
+}
